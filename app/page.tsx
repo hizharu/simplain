ebd3c032719e002a4b2b7e92bbcc7e68cc9ab60c
hizharu <muhammadaprizalsanjaya@gmail.com>
2026-01-20 18:00:17 +0700

    fixing position for hero section

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,29 @@ export default function Home() {
     <header>
       <nav className="bg-dark">
         <ul className="bg-white/20 container mx-auto h-[60px] w-[700px] rounded-full flex items-center justify-center gap-12 text-white fixed inset-0">
-            <li>Logo</li>
-            <li>Home</li>
-            <li>Fundamental</li>
-            <li>Explain Me</li>
-            <li>Quiz</li>
-            <li>Account</li>
+            <li className="cursor-pointer">Logo</li>
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">Fundamental</li>
+            <li className="cursor-pointer">Explain Me</li>
+            <li className="cursor-pointer">Quiz</li>
+            <li className="cursor-pointer">Account</li>
         </ul>
       </nav>
     </header>
 
-    <section className="container mx-auto pt-[80px]">
-      <h1 className="text-white text-[48px] font-medium">Let’s explain it simply</h1>
-      <p className="text-[18px] font-medium">Guide you with simple explanations, real-life examples, <br />
+    <section className="mt-40 container pt-[80px] ml-[80px] justify-between">
+      <div className="max-w-2xl">
+      <h1 className="text-white text-[48px] font-medium leading-tight">
+        Let’s explain it <span className="text-black">simply</span>
+      </h1>
+      <p className="text-[18px] font-medium leading-relaxed">Guide you with simple explanations, real-life examples, <br />
         and insights that actually stick <br />
-        Because this world is full of the complex things</p>
-        <button className="btn">Get Started</button>
+        Because this world is full of the <span className="text-white">complex things</span>
+      </p>
+      <button className="flex items-center justify-center gap-2 px-[18px] py-[9px] rounded-full bg-white text-black-[14px] font-medium mt-10">
+        Get Started
+      </button>
+      </div>
     </section>
   </main>
   );
